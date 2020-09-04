a488218ca192d40deca95265af64c4e9ef9a6339
Wellinton Reis <wellinton_fabricio@hotmail.com>
2020-09-03 23:55:49 -0300

    fix correspondence repository

--- src/repositories/CorrespondencesRepository.ts
+++ src/repositories/CorrespondencesRepository.ts
@@ -1,3 +1,17 @@
-class CorrespondencesRepository {}
+import { EntityRepository, Repository, Like } from 'typeorm';
+import Correspondence from '../models/Correspondence';
+
+@EntityRepository(Correspondence)
+class CorrespondencesRepository extends Repository<Correspondence> {
+  public async findByRecipientName(
+    name: string
+  ): Promise<Correspondence | null> {
+    const findCorrespondence = await this.findOne({
+      where: { recipient_name: Like(`%${name}%`) },
+    });
+
+    return findCorrespondence || null;
+  }
+}
 
 export default CorrespondencesRepository;
