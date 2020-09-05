068ce88308f4032aff2bd2f2babd47175e9c5013
Wellinton Reis <wellinton_fabricio@hotmail.com>
2020-09-05 00:21:15 -0300

    create route to users

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import correspondencesRouter from './correspondences.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/correspondences', correspondencesRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
