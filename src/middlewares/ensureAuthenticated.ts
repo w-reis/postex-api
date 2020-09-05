65d57b7cb9c1ca6b3bdcb70ffe34055c3cb6a8da
Wellinton Reis <wellinton_fabricio@hotmail.com>
2020-09-05 18:56:23 -0300

    append user info  to request

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -2,6 +2,12 @@ import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 import authConfig from '../config/auth';
 
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
 export default function ensureAuthenticated(
   request: Request,
   response: Response,
@@ -18,7 +24,11 @@ export default function ensureAuthenticated(
   try {
     const decoded = verify(token, authConfig.jwt.secret);
 
-    console.log(decoded);
+    const { sub } = decoded as TokenPayload;
+
+    request.user = {
+      id: parseInt(sub, 10),
+    };
 
     return next();
   } catch {
