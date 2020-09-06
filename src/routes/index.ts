1d109dee1a7c3fadd0b26951544b5e9b43406506
Wellinton Reis <wellinton_fabricio@hotmail.com>
2020-09-05 22:21:42 -0300

    create route to recipients

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import correspondencesRouter from './correspondences.routes';
 import usersRouter from './users.routes';
+import recipientsRouter from './recipients.routes';
 import sessionsRouter from './sessions.routes';
 
 const routes = Router();
 
 routes.use('/correspondences', correspondencesRouter);
 routes.use('/users', usersRouter);
+routes.use('/recipients', recipientsRouter);
 routes.use('/sessions', sessionsRouter);
 
 export default routes;
---
+++ src/routes/recipients.routes.ts
@@ -0,0 +1,41 @@
+import { Router } from 'express';
+
+import CreateRecipientService from '../services/CreateRecipientService';
+
+const recipientsRouter = Router();
+
+recipientsRouter.post('/', async (request, response) => {
+  const {
+    name,
+    email,
+    password,
+    address,
+    number,
+    complement,
+    neighborhood,
+    city,
+    state,
+    CEP,
+  } = request.body;
+
+  const createRecipient = new CreateRecipientService();
+
+  const recipient = await createRecipient.excute({
+    name,
+    email,
+    password,
+    address,
+    number,
+    complement,
+    neighborhood,
+    city,
+    state,
+    CEP,
+  });
+
+  delete recipient.password;
+
+  return response.json(recipient);
+});
+
+export default recipientsRouter;
