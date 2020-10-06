14d2afee32576a2cedc9aace3fd1d6bf70c6506a
Wellinton Reis <wellinton_fabricio@hotmail.com>
2020-10-06 20:07:02 -0300

    creates controller to correspondences

---
+++ src/controllers/CorrespondencesController.ts
@@ -0,0 +1,82 @@
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+
+import CorrespondencesRepository from '../repositories/CorrespondencesRepository';
+
+import CreateCorrespondenceService from '../services/correspondence/CreateCorrespondenceService';
+import UpdateCorrespondenceService from '../services/correspondence/UpdateCorrespondenceService';
+import DeliverCorrespondenceService from '../services/correspondence/DeliveCorrespondenceService';
+
+export default class CorrespondencesController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    return response.json(response.paginatedResults);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { recipient_name, recipient_id, object_number } = request.body;
+
+    const createCorrespondence = new CreateCorrespondenceService();
+
+    await createCorrespondence.execute({
+      recipient_name,
+      recipient_id,
+      object_number,
+      status: 'pendente',
+    });
+
+    return response.sendStatus(200);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { recipient_name, recipient_id, object_number } = request.body;
+    const { id } = request.params;
+
+    const updateCorrespondence = new UpdateCorrespondenceService();
+
+    const correspondence = await updateCorrespondence.execute({
+      id,
+      recipient_name,
+      recipient_id,
+      object_number,
+    });
+
+    return response.json(correspondence);
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const { idGroup }: { idGroup: string[] } = request.query;
+    const correspondencesRepository = getCustomRepository(
+      CorrespondencesRepository
+    );
+
+    await correspondencesRepository.deleteCorrespondence(idGroup);
+    return response.sendStatus(200);
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const correspondencesRepository = getCustomRepository(
+      CorrespondencesRepository
+    );
+    const correspondence = await correspondencesRepository.showCorrespondence(
+      id
+    );
+
+    return response.json(correspondence);
+  }
+
+  public async deliver(
+    request: Request,
+    response: Response
+  ): Promise<Response> {
+    const { id } = request.params;
+
+    const deliverCorrespondence = new DeliverCorrespondenceService();
+
+    const correspondence = await deliverCorrespondence.execute({
+      id,
+    });
+
+    return response.json(correspondence);
+  }
+}
--- src/routes/correspondences.routes.ts
+++ src/routes/correspondences.routes.ts
@@ -1,87 +1,29 @@
 import { Router } from 'express';
-import { getCustomRepository } from 'typeorm';
-
-import CreateCorrespondenceService from '../services/correspondence/CreateCorrespondenceService';
-import CorrespondencesRepository from '../repositories/CorrespondencesRepository';
 
 import paginateCorrespondenceResult from '../middlewares/paginateCorrespondencesResult';
 import ensureUserAuthenticated from '../middlewares/ensureUserAuthenticated';
-import UpdateCorrespondenceService from '../services/correspondence/UpdateCorrespondenceService';
-import DeliverCorrespondenceService from '../services/correspondence/DeliveCorrespondenceService';
+import CorrespondencesController from '../controllers/CorrespondencesController';
 
 const correspondencesRouter = Router();
+
+const correspondencesController = new CorrespondencesController();
 
 correspondencesRouter.use(ensureUserAuthenticated);
 
 correspondencesRouter.get(
   '/',
   paginateCorrespondenceResult,
-  async (request, response) => {
-    return response.json(response.paginatedResults);
-  }
+  correspondencesController.index
 );
 
-correspondencesRouter.get('/:id', async (request, response) => {
-  const { id } = request.params;
-  const correspondencesRepository = getCustomRepository(
-    CorrespondencesRepository
-  );
-  const correspondence = await correspondencesRepository.showCorrespondence(id);
+correspondencesRouter.get('/:id', correspondencesController.show);
 
-  return response.json(correspondence);
-});
+correspondencesRouter.post('/', correspondencesController.create);
 
-correspondencesRouter.post('/', async (request, response) => {
-  const { recipient_name, recipient_id, object_number } = request.body;
+correspondencesRouter.put('/:id', correspondencesController.update);
 
-  const createCorrespondence = new CreateCorrespondenceService();
+correspondencesRouter.patch('/:id', correspondencesController.deliver);
 
-  await createCorrespondence.execute({
-    recipient_name,
-    recipient_id,
-    object_number,
-    status: 'pendente',
-  });
-
-  return response.sendStatus(200);
-});
-
-correspondencesRouter.put('/:id', async (request, response) => {
-  const { recipient_name, recipient_id, object_number } = request.body;
-  const { id } = request.params;
-
-  const updateCorrespondence = new UpdateCorrespondenceService();
-
-  const correspondence = await updateCorrespondence.execute({
-    id,
-    recipient_name,
-    recipient_id,
-    object_number,
-  });
-
-  return response.json(correspondence);
-});
-
-correspondencesRouter.patch('/:id', async (request, response) => {
-  const { id } = request.params;
-
-  const deliverCorrespondence = new DeliverCorrespondenceService();
-
-  const correspondence = await deliverCorrespondence.execute({
-    id,
-  });
-
-  return response.json(correspondence);
-});
-
-correspondencesRouter.delete('/', async (request, response) => {
-  const { idGroup }: { idGroup: string[] } = request.query;
-  const correspondencesRepository = getCustomRepository(
-    CorrespondencesRepository
-  );
-
-  await correspondencesRepository.deleteCorrespondence(idGroup);
-  return response.sendStatus(200);
-});
+correspondencesRouter.delete('/', correspondencesController.delete);
 
 export default correspondencesRouter;
