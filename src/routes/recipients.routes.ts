d3392aa4a7efff5d7d0ea9a8bbfde4a61a06b355
Wellinton Reis <wellinton_fabricio@hotmail.com>
2020-10-10 23:49:55 -0300

    adds ath middleware for users routes

--- src/routes/recipients.routes.ts
+++ src/routes/recipients.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
 import RecipientsController from '../controllers/RecipientsController';
+import ensureUserAuthenticated from '../middlewares/ensureUserAuthenticated';
 
 const recipientsRouter = Router();
 
 const recipientsController = new RecipientsController();
+
+recipientsRouter.use(ensureUserAuthenticated);
 
 recipientsRouter.get('/', recipientsController.index);
 
