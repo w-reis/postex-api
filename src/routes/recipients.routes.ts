cf8957be6d6bbecf406c51fe9f8d78fce96e04c0
Wellinton Reis <wellinton_fabricio@hotmail.com>
2020-12-01 16:08:34 -0300

    fix: recipeint authentication condition

--- src/routes/recipients.routes.ts
+++ src/routes/recipients.routes.ts
@@ -1,16 +1,18 @@
 import { Router } from 'express';
 
 import RecipientsController from '../controllers/RecipientsController';
-import ensureUserAuthenticated from '../middlewares/ensureUserAuthenticated';
+import ensureRecipientAuthenticated from '../middlewares/ensureRecipientAuthenticated';
 
 const recipientsRouter = Router();
 
 const recipientsController = new RecipientsController();
 
-recipientsRouter.get('/', ensureUserAuthenticated, recipientsController.index);
+recipientsRouter.post('/', recipientsController.create);
+
+recipientsRouter.use(ensureRecipientAuthenticated);
+
+recipientsRouter.get('/', recipientsController.index);
 
 recipientsRouter.get('/:id', recipientsController.show);
-
-recipientsRouter.post('/', recipientsController.create);
 
 export default recipientsRouter;
